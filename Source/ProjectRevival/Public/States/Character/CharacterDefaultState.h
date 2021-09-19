55d67e387d9fa57d5aaf3b02abab8bf6abe177eb
Nikita Kuznetsov <ndkuznetsov_1@edu.hse.ru>
2021-09-19 13:02:40 +0300

    Created additional character states

---
+++ Source/ProjectRevival/Public/States/Character/CharacterDefaultState.h
@@ -0,0 +1,15 @@
+﻿// Project Revival. All Rights Reserved
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "States/PermanentState.h"
+#include "CharacterDefaultState.generated.h"
+
+
+
+UCLASS()
+class PROJECTREVIVAL_API UCharacterDefaultState : public UPermanentState
+{
+	GENERATED_BODY()
+};
---
+++ Source/ProjectRevival/Public/States/Character/CharacterJumpState.h
@@ -0,0 +1,14 @@
+﻿// Project Revival. All Rights Reserved
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "States/TemporaryState.h"
+#include "CharacterJumpState.generated.h"
+
+
+UCLASS()
+class PROJECTREVIVAL_API UCharacterJumpState : public UTemporaryState
+{
+	GENERATED_BODY()
+};
