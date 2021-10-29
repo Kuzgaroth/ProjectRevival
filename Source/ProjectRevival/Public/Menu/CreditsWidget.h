f32137e3c58fa902f02cb793bbd0af979737d608
Yulia Melkina <koluchikrot@gmail.com>
2021-10-29 19:44:31 +0300

    Added menu buttons, created OptionsWidget and CreditsWidget, added transitions, redesigned the whole layout

---
+++ Source/ProjectRevival/Public/Menu/CreditsWidget.h
@@ -0,0 +1,30 @@
+// Project Revival. All Rights Reserved
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Blueprint/UserWidget.h"
+#include "CreditsWidget.generated.h"
+
+class UButton;
+/**
+ * 
+ */
+UCLASS()
+class PROJECTREVIVAL_API UCreditsWidget : public UUserWidget
+{
+	GENERATED_BODY()
+
+	protected:
+	UPROPERTY(meta=(BindWidget))
+	UButton* BackButton;
+
+	UPROPERTY(EditDefaultsOnly, BlueprintReadWrite, Category="UI")
+	TSubclassOf<UUserWidget> MenuWidgetClass;
+
+	virtual void NativeOnInitialized() override;
+
+	private:
+	UFUNCTION()
+	void OnBack();
+};
