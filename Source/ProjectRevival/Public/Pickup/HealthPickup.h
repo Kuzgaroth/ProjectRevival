1665d40a87ed79eb4a356897e89de72e0944cb9a
fazilhus <khusnutdinov.fazil@gmail.com>
2021-11-04 16:56:39 +0300

    interface realised

--- Source/ProjectRevival/Public/Pickup/HealthPickup.h
+++ Source/ProjectRevival/Public/Pickup/HealthPickup.h
@@ -4,14 +4,20 @@
 
 #include "CoreMinimal.h"
 #include "Pickup/BasePickup.h"
+#include "Interfaces/BotPickupInterface.h"
 #include "HealthPickup.generated.h"
 
 
 
 UCLASS()
-class PROJECTREVIVAL_API AHealthPickup : public ABasePickup
+class PROJECTREVIVAL_API AHealthPickup : public ABasePickup, public IBotPickupInterface
 {
 	GENERATED_BODY()
+
+public:
+	UFUNCTION(BlueprintNativeEvent, BlueprintCallable, Category="Interaction")
+	void OnInteraction(AActor* Actor);
+	virtual void OnInteraction_Implementation(AActor* Actor);
 protected:
 	UPROPERTY(EditAnywhere,BlueprintReadWrite, Category="Pickup", meta=(ClampMin="1.0", ClampMax="100.0"))
 	float HealthAmount = 50.0f;
