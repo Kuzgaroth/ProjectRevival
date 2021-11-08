bc12edcab9cc5dd66407465776a6bd4b11f095d9
Yulia Melkina <koluchikrot@gmail.com>
2021-11-08 16:31:19 +0300

    Added transitions and resolution options

--- Source/ProjectRevival/Private/Menu/OptionsWidget.cpp
+++ Source/ProjectRevival/Private/Menu/OptionsWidget.cpp
@@ -3,6 +3,8 @@
 
 #include "Menu/OptionsWidget.h"
 #include "Components/Button.h"
+#include "Menu/OptionsControlsWidget.h"
+#include "Menu/OptionsGraphicsWidget.h"
 #include "ProjectRevival/Public/Menu/MenuWidget.h"
 
 void UOptionsWidget::NativeOnInitialized()
@@ -13,6 +15,24 @@ void UOptionsWidget::NativeOnInitialized()
 	{
 		BackButton->OnClicked.AddDynamic(this, &UOptionsWidget::OnBack);
 	}
+	if (ControlsButton)
+	{
+		ControlsButton->OnClicked.AddDynamic(this, &UOptionsWidget::OnControls);
+	}
+	if (GraphicsButton)
+	{
+		GraphicsButton->OnClicked.AddDynamic(this, &UOptionsWidget::OnGraphics);
+	}
+	if (OptionsControlsWidgetClass)
+	{
+		UOptionsControlsWidget* OptionsControlsWidget = CreateWidget<UOptionsControlsWidget>(GetWorld(), OptionsControlsWidgetClass);
+		OptionsWidgetSwitcher->AddChild(OptionsControlsWidget);
+	}
+	if (OptionsGraphicsWidgetClass)
+	{
+		UOptionsGraphicsWidget* OptionsGraphicsWidget = CreateWidget<UOptionsGraphicsWidget>(GetWorld(), OptionsGraphicsWidgetClass);
+		OptionsWidgetSwitcher->AddChild(OptionsGraphicsWidget);
+	}
 }
 
 void UOptionsWidget::OnBack()
@@ -22,6 +42,22 @@ void UOptionsWidget::OnBack()
 		RemoveFromParent();
 		UMenuWidget* MenuWidget = CreateWidget<UMenuWidget>(GetWorld(), MenuWidgetClass);
 		MenuWidget->AddToViewport();
+	}
+}
+
+void UOptionsWidget::OnControls()
+{
+	if (OptionsControlsWidgetClass)
+	{
+		OptionsWidgetSwitcher->SetActiveWidgetIndex(0);
+	}
+}
+
+void UOptionsWidget::OnGraphics()
+{
+	if (OptionsGraphicsWidgetClass)
+	{
+		OptionsWidgetSwitcher->SetActiveWidgetIndex(1);
 	}
 }
 
