15a084dcf025e487f816086596a85d0fc9e86f4b
Nikita Kuznetsov <ndkuznetsov_1@edu.hse.ru>
2021-11-18 16:10:54 +0300

    Created Task file for ghost ability

---
+++ Source/ProjectRevival/Private/AbilitySystem/AbilityTasks/GhostMainTask.cpp
@@ -0,0 +1,5 @@
+// Project Revival. All Rights Reserved
+
+
+#include "AbilitySystem/AbilityTasks/GhostMainTask.h"
+
---
+++ Source/ProjectRevival/Public/AbilitySystem/AbilityTasks/GhostMainTask.h
@@ -0,0 +1,16 @@
+// Project Revival. All Rights Reserved
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Abilities/Tasks/AbilityTask.h"
+#include "GhostMainTask.generated.h"
+
+
+
+UCLASS()
+class PROJECTREVIVAL_API UGhostMainTask : public UAbilityTask
+{
+	GENERATED_BODY()
+	
+};
