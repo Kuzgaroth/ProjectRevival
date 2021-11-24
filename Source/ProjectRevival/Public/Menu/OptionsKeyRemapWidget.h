75d72245aa8c422ea9b5a84a151cd67fbe007cc1
Yulia Melkina <koluchikrot@gmail.com>
2021-11-24 22:40:41 +0300

    Added functional for remapping game input keys

---
+++ Source/ProjectRevival/Public/Menu/OptionsKeyRemapWidget.h
@@ -0,0 +1,46 @@
+// Project Revival. All Rights Reserved
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Blueprint/UserWidget.h"
+#include "Components/TextBlock.h"
+#include "GameFramework/PlayerInput.h"
+
+#include "OptionsKeyRemapWidget.generated.h"
+
+class UButton;
+/**
+ * 
+ */
+UCLASS()
+class PROJECTREVIVAL_API UOptionsKeyRemapWidget : public UUserWidget
+{
+	GENERATED_BODY()
+
+public:
+	UPROPERTY(meta=(BindWidget))
+	UButton* ChangeInputButton;
+
+	UPROPERTY(meta=(BindWidget))
+	UTextBlock* ActionText;
+
+	UPROPERTY(meta=(BindWidget))
+	UTextBlock* KeyText;
+
+	virtual void NativeOnInitialized() override;
+
+	UFUNCTION()
+	void SetContent(const FInputActionKeyMapping KeyMapping);
+	
+	virtual FReply NativeOnKeyDown(const FGeometry& InGeometry, const FKeyEvent& InKeyEvent) override;
+	
+private:
+	bool bCanInput;
+
+	FInputActionKeyMapping KeyMap;
+	
+	UFUNCTION()
+    void OnChangeInput();
+	
+};
