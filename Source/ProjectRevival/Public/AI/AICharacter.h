9b5f59935ce7d22a82a21e33f37ea1334143eca7
fazilhus <khusnutdinov.fazil@gmail.com>
2021-11-25 20:23:32 +0300

    Система оценки ситуации бота
    Баг - Работает для всех ботов кроме самого первого

--- Source/ProjectRevival/Public/AI/AICharacter.h
+++ Source/ProjectRevival/Public/AI/AICharacter.h
@@ -3,11 +3,22 @@
 #pragma once
 
 #include "CoreMinimal.h"
+#include "BaseAIController.h"
+#include "EnvironmentQuery/EQSTestingPawn.h"
 #include "Player/BaseCharacter.h"
 #include "AICharacter.generated.h"
 
 class UBehaviorTree;
 class UWidgetComponent;
+
+UENUM(BlueprintType)
+enum class HealthState: uint8
+{
+	NEAR_DEATH,
+	BADLY_INJURED,
+	SLIGHTLY_INJURED,
+	FULL_HEALTH
+};
 
 UCLASS()
 class PROJECTREVIVAL_API AAICharacter : public ABaseCharacter
@@ -16,19 +27,34 @@ class PROJECTREVIVAL_API AAICharacter : public ABaseCharacter
 public:
 	AAICharacter(const FObjectInitializer& ObjectInitializer);
 
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Status")
+	HealthState HState;
+
 	UPROPERTY(EditDefaultsOnly, BlueprintReadWrite, Category = "AI")
 	UBehaviorTree* BehaviorTreeAsset;
+
+	UPROPERTY(EditDefaultsOnly, BlueprintReadWrite, Category = "AI")
+	ABaseAIController* AICon;
+	
+	UPROPERTY(EditDefaultsOnly, BlueprintReadWrite, Category = "AI")
+	UBlackboardComponent* BBComp;
+
+	UPROPERTY(EditDefaultsOnly, BlueprintReadWrite, Category = "AI")
+	FName HealthStatus = "HStatus";
+	
 	virtual void OnDeath() override;
 	virtual void Tick(float DeltaSeconds) override;
+	virtual void PossessedBy(AController* NewController) override;
 protected:
 	UPROPERTY(VisibleAnywhere, BlueprintReadWrite, Category = "Components")
 	UWidgetComponent* HealthWidgetComponent;
 
 	UPROPERTY(EditDefaultsOnly, BlueprintReadWrite, Category = "AI")
-	float HealthVisibilityDistance  = 1000.0f;
+	float HealthVisibilityDistance  = 2000.0f;
 	
 	virtual void BeginPlay() override;
 	virtual void OnHealthChanged(float CurrentHealth, float HealthDelta) override;
 private:
 	void UpdateHealthWidgetVisibility();
+	void UpdateHStateBlackboardKey(uint8 EnumKey);
 };
