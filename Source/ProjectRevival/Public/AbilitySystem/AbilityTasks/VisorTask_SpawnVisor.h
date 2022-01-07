f4800604c761b73ce3c60069d1e488a3db803488
Nikita Kuznetsov <ndkuznetsov_1@edu.hse.ru>
2022-01-07 20:11:23 +0300

    Complete Visor changes (#42)
    Co-authored-by: Ruslan Ruziev <riruziev@edu.hse.ru>

---
+++ Source/ProjectRevival/Public/AbilitySystem/AbilityTasks/VisorTask_SpawnVisor.h
@@ -0,0 +1,45 @@
+// Project Revival. All Rights Reserved
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Abilities/Tasks/AbilityTask.h"
+#include "VisorTask_SpawnVisor.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class PROJECTREVIVAL_API UVisorTask_SpawnVisor : public UAbilityTask
+{
+	GENERATED_BODY()
+
+public:
+	virtual void Activate() override;
+
+	UFUNCTION(BlueprintCallable, Category = "Ability|Tasks", meta = (HidePin = "OwningAbility", DefaultToSelf = "OwningAbility"))
+	static UVisorTask_SpawnVisor* SpawnVisor(UGameplayAbility* OwningAbility, float VisorRadius, float VisorDuration,
+		TArray<TEnumAsByte<EObjectTypeQuery>> ObjectTypesToVisor, UParticleSystem* ParticleEffect);
+	
+	//The range in which enemies and objects are visored 
+	UPROPERTY()
+	float Radius;
+	
+	//Lifetime of ability
+	UPROPERTY()
+	float Duration;
+
+	UPROPERTY()
+	UGameplayAbility* AbilityHead;
+
+	//Types of collisions by which objects are going to be visored
+	UPROPERTY()
+	TArray<TEnumAsByte<EObjectTypeQuery>> ObjectTypesForVisoring;
+
+	// Particle effect spawned visor point (item)
+	UPROPERTY()
+	UParticleSystem* EffectToShow;
+	
+private:
+	virtual void OnDestroy(bool AbilityEnded) override;
+};
