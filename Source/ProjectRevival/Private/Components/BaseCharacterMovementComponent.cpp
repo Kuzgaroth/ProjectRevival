084b48f08498523be4e81a62c8beeb0735ab3787
Nikita Kuznetsov <ndkuznetsov_1@edu.hse.ru>
2022-01-22 23:34:56 +0300

    Changed movement modifier to 0.75 in aim mode

--- Source/ProjectRevival/Private/Components/BaseCharacterMovementComponent.cpp
+++ Source/ProjectRevival/Private/Components/BaseCharacterMovementComponent.cpp
@@ -53,7 +53,7 @@ float UBaseCharacterMovementComponent::GetMaxSpeed() const
 	const float MaxSpeed = Super::GetMaxSpeed();
 	const ABaseCharacter* Player = Cast<ABaseCharacter>(GetPawnOwner());
 	if (Player && Player->IsRunning()) return MaxSpeed*RunModifier;
-	if (PlayerMovementLogic.IsPivotTargeted) return MaxSpeed*0.5f;
+	if (PlayerMovementLogic.IsPivotTargeted) return MaxSpeed*0.75f;
 	return MaxSpeed;
 	
 }
