e3608e81abcac62991c211ac7a6d91adc4b71c67
fazilhus <35606950+fazilhus@users.noreply.github.com>
2022-02-11 11:07:40 +0300

    Ai soldier fire (#84)
    * Finally I've read and understood most of the newest AI code and now I think I see how to implement functions correctly to work with what Fazil made
    
    * Cover system code base for Soldier
    
    * Выполнил issue #72
    
    Co-authored-by: Ruslan Ruziev <riruziev@edu.hse.ru>

---
+++ Source/ProjectRevival/Public/AI/Soldier/Tasks/BTT_FindNewCover.h
@@ -0,0 +1,19 @@
+// Project Revival. All Rights Reserved
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "BehaviorTree/BTTaskNode.h"
+#include "BTT_FindNewCover.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class PROJECTREVIVAL_API UBTT_FindNewCover : public UBTTaskNode
+{
+	GENERATED_BODY()
+	UBTT_FindNewCover();
+	virtual EBTNodeResult::Type ExecuteTask(UBehaviorTreeComponent& OwnerComp, uint8* NodeMemory) override;
+	
+};
