864da0ee91910cf6c1653dd02c606efcf962f865
Ruslan Ruziev <riruziev@edu.hse.ru>
2022-02-13 01:13:26 +0300

    Fixed some unexpected issues. Minor code and bp changes. Added extra high cover animations, fixed some old animations and blendspaces.

--- Source/ProjectRevival/Private/AI/SoldierEnemy.cpp
+++ Source/ProjectRevival/Private/AI/SoldierEnemy.cpp
@@ -4,7 +4,7 @@
 
 
 #include "AI/SoldierEnemy.h"
-#include "BaseAIController.h"
+#include "AICharacter.h"
 #include "GameFramework/CharacterMovementComponent.h"
 #include "AIWeaponComponent.h"
 #include "BrainComponent.h"
@@ -17,31 +17,21 @@
 #include "Kismet/GameplayStatics.h"
 #include "Soldier/SoldierAIController.h"
 
-ASoldierEnemy::ASoldierEnemy(const FObjectInitializer& ObjectInitializer) :Super(
-	ObjectInitializer.SetDefaultSubobjectClass<UAIWeaponComponent>("WeaponComponent"))
+void ASoldierEnemy::BeginPlay()
 {
-	AutoPossessAI = EAutoPossessAI::Disabled;
-	AIControllerClass = ABaseAIController::StaticClass();
-	bUseControllerRotationYaw = false;
-	if (GetCharacterMovement())
-	{
-		GetCharacterMovement()->bUseControllerDesiredRotation = true;
-		GetCharacterMovement()->RotationRate = FRotator(0.0f, 200.0f, 0.0f);
-	}
-
-	HealthWidgetComponent = CreateDefaultSubobject<UWidgetComponent>("WidgetComponent");
-	HealthWidgetComponent->SetupAttachment(RootComponent);
-	HealthWidgetComponent->SetWidgetSpace(EWidgetSpace::Screen);
-	HealthWidgetComponent->SetDrawAtDesiredSize(true);
-
+	Super::BeginPlay();
 	SideMoveAmount = 0;
-	
-	HState = HealthStateSoldier::FULL_HEALTH;
+	CoverData.IsTurning = false;
+	CoverData.IsInCoverTransition = false;
+	CoverData.IsInFireTransition = false;
+	CoverData.IsSwitchingCoverType = false;
+	CoverData.IsFiring = false;
+	bIsInCoverBP = false;
 }
 
 void ASoldierEnemy::OnDeath()
 {
-	Super::OnDeath();
+	ABaseCharacter::OnDeath();
 
 	const auto PRController = Cast<ASoldierAIController>(GetController());
 	if (PRController && PRController->BrainComponent)
@@ -52,72 +42,20 @@ void ASoldierEnemy::OnDeath()
 
 void ASoldierEnemy::Tick(float DeltaSeconds)
 {
-	UpdateHealthWidgetVisibility();
+	Super::Tick(DeltaSeconds);
 }
 
 void ASoldierEnemy::PossessedBy(AController* NewController)
 {
 	Super::PossessedBy(NewController);
-	AICon = Cast<ASoldierAIController>(GetController());
-	UE_LOG(LogTemp, Log, TEXT("After AICon cast"));
-	if (AICon)
-	{
-		BBComp = AICon->GetBlackboardComponent();
-		UE_LOG(LogTemp, Log, TEXT("Check if cast was successful"));
-	}
-	UpdateHStateBlackboardKey(3);
-	UE_LOG(LogTemp, Log, TEXT("SPECIAL"));
 	Cast<ASoldierAIController>(GetController())->StartEnteringCoverDelegate.AddDynamic(this, &ASoldierEnemy::StartCoverSoldier);
 	Cast<ASoldierAIController>(GetController())->StartExitingCoverDelegate.AddDynamic(this, &ASoldierEnemy::StopCoverSoldier);
 	Cast<ASoldierAIController>(GetController())->StartCoverSideMovingDelegate.AddDynamic(this, &ASoldierEnemy::ChangeCoverSide);
 }
 
-void ASoldierEnemy::BeginPlay()
-{
-	Super::BeginPlay();
-	check(HealthWidgetComponent);
-	CoverData.IsTurning = false;
-	CoverData.IsInCoverTransition = false;
-	CoverData.IsInFireTransition = false;
-	CoverData.IsSwitchingCoverType = false;
-	CoverData.IsFiring = false;
-	bIsInCoverBP = false;
-}
-
 void ASoldierEnemy::OnHealthChanged(float CurrentHealth, float HealthDelta)
 {
 	Super::OnHealthChanged(CurrentHealth, HealthDelta);
-
-	if (HealthComponent->GetHealthPercentage() > 0.75)
-	{
-		HState = HealthStateSoldier::FULL_HEALTH;
-		UE_LOG(LogTemp, Log, TEXT("HState has changed %i"), static_cast<uint8>(HState));
-		UpdateHStateBlackboardKey(3);
-	}
-	else if ((HealthComponent->GetHealthPercentage() <= 0.75) && (HealthComponent->GetHealthPercentage() > 0.5))
-	{
-		HState = HealthStateSoldier::SLIGHTLY_INJURED;
-		UE_LOG(LogTemp, Log, TEXT("HState has changed %i"), static_cast<uint8>(HState));
-		UpdateHStateBlackboardKey(2);
-	}
-	else if ((HealthComponent->GetHealthPercentage() <= 0.5) && (HealthComponent->GetHealthPercentage() > 0.25))
-	{
-		HState = HealthStateSoldier::BADLY_INJURED;
-		UE_LOG(LogTemp, Log, TEXT("HState has changed %i"), static_cast<uint8>(HState));
-		UpdateHStateBlackboardKey(1);
-	}
-	else
-	{
-		HState = HealthStateSoldier::NEAR_DEATH;
-		UE_LOG(LogTemp, Log, TEXT("HState has changed %i"), static_cast<uint8>(HState));
-		UpdateHStateBlackboardKey(0);
-	}
-	
-	const auto HealthBarWidget = Cast<UHealthBarWidget>(HealthWidgetComponent->GetUserWidgetObject());
-
-	if (!HealthBarWidget) return;
-
-	HealthBarWidget->SetHealthPercent(HealthComponent->GetHealthPercentage());
 }
 
 void ASoldierEnemy::UpdateHStateBlackboardKey(uint8 EnumKey)
--- Source/ProjectRevival/Public/AI/SoldierEnemy.h
+++ Source/ProjectRevival/Public/AI/SoldierEnemy.h
@@ -3,9 +3,8 @@
 #pragma once
 
 #include "CoreMinimal.h"
-#include "BaseAIController.h"
+#include "AICharacter.h"
 #include "EnvironmentQuery/EQSTestingPawn.h"
-#include "Player/BaseCharacter.h"
 #include "Soldier/SoldierAIController.h"
 #include "SoldierEnemy.generated.h"
 
@@ -16,39 +15,11 @@ DECLARE_DYNAMIC_MULTICAST_DELEGATE(FStopEnteringCover);
 DECLARE_DYNAMIC_MULTICAST_DELEGATE(FStopExitingCover);
 DECLARE_DYNAMIC_MULTICAST_DELEGATE(FStopCoverSideMoving);
 
-class UBehaviorTree;
-class UWidgetComponent;
-
-UENUM(BlueprintType)
-enum class HealthStateSoldier: uint8
-{
-	NEAR_DEATH,
-	BADLY_INJURED,
-	SLIGHTLY_INJURED,
-	FULL_HEALTH
-};
-
 UCLASS()
-class PROJECTREVIVAL_API ASoldierEnemy : public ABaseCharacter, public IICoverable
+class PROJECTREVIVAL_API ASoldierEnemy : public AAICharacter, public IICoverable
 {
 	GENERATED_BODY()
 public:
-	ASoldierEnemy(const FObjectInitializer& ObjectInitializer);
-
-	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Status")
-	HealthStateSoldier HState;
-
-	UPROPERTY(EditDefaultsOnly, BlueprintReadWrite, Category = "AI")
-	UBehaviorTree* BehaviorTreeAsset;
-
-	UPROPERTY(EditDefaultsOnly, BlueprintReadWrite, Category = "AI")
-	ASoldierAIController* AICon;
-	
-	UPROPERTY(EditDefaultsOnly, BlueprintReadWrite, Category = "AI")
-	UBlackboardComponent* BBComp;
-
-	UPROPERTY(EditDefaultsOnly, BlueprintReadWrite, Category = "AI")
-	FName HealthStatus = "HStatus";
 	
 	virtual void OnDeath() override;
 	virtual void Tick(float DeltaSeconds) override;
@@ -91,14 +62,12 @@ public:
 	void ChangeCoverSideFinish();
 
 	UPROPERTY(BlueprintReadOnly)
-	bool bIsInCoverBP; 
+	bool bIsInCoverBP;
+
+	UPROPERTY(BlueprintReadOnly)
+	FCoverData CoverData;
 	
 protected:
-	UPROPERTY(VisibleAnywhere, BlueprintReadWrite, Category = "Components")
-	UWidgetComponent* HealthWidgetComponent;
-
-	UPROPERTY(EditDefaultsOnly, BlueprintReadWrite, Category = "AI")
-	float HealthVisibilityDistance  = 2000.0f;
 	
 	virtual void BeginPlay() override;
 	virtual void OnHealthChanged(float CurrentHealth, float HealthDelta) override;
@@ -107,12 +76,10 @@ protected:
 	virtual TEnumAsByte<ECoverPart> GetCoverPart(int8 PartPos);
 
 	float SideMoveAmount;
+	
 
 private:
 	void UpdateHealthWidgetVisibility();
 	void UpdateHStateBlackboardKey(uint8 EnumKey);
-
-	//UPROPERTY()
-	FCoverData CoverData;
 	void CoverCrouch();
 };
